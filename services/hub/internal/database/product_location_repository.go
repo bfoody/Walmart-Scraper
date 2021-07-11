57d894e51d5a29479ff5671842a0d55bfa89caa2
Tyler Lafayette <tylerlafayette@gmail.com>
2021-07-10 23:09:06 -0700

    Implementations for ProductRepository and ProductLocationRepository

---
+++ services/hub/internal/database/product_location_repository.go
@@ -0,0 +1,99 @@
+package database
+
+import (
+	"github.com/bfoody/Walmart-Scraper/domain"
+	"github.com/bfoody/Walmart-Scraper/utils/uuid"
+	"github.com/jmoiron/sqlx"
+)
+
+// A ProductLocationRepository provides methods for interacting with ProductLocations in the
+// database.
+type ProductLocationRepository struct {
+	db *sqlx.DB
+}
+
+// NewProductLocationRepository creates and returns a *ProductLocationRepository with the supplied
+// database connection.
+func NewProductLocationRepository(db *sqlx.DB) *ProductLocationRepository {
+	return &ProductLocationRepository{db}
+}
+
+// FindProductLocationByID finds a single product location by ID, returning an error
+// if nothing is found.
+func (r *ProductLocationRepository) FindProductLocationByID(id string) (*domain.ProductLocation, error) {
+	var productLocation *domain.ProductLocation
+	err := r.db.Get(productLocation, "SELECT * FROM product_locations WHERE id=$1", id)
+	if err != nil {
+		return nil, err
+	}
+
+	return productLocation, nil
+}
+
+// FindProductLocationsByProductID finds multiple product locations by product ID,
+// returning an empty array if nothing is found.
+func (r *ProductLocationRepository) FindProductLocationsByProductID(id string) ([]domain.ProductLocation, error) {
+	var productLocations []domain.ProductLocation
+	err := r.db.Select(productLocations, "SELECT * FROM product_locations WHERE product_id=$1", id)
+	if err != nil {
+		return nil, err
+	}
+
+	return productLocations, nil
+}
+
+// FindProductLocationsByLocationID finds multiple product locations by location ID,
+// returning an empty array if nothing is found.
+func (r *ProductLocationRepository) FindProductLocationsByLocationID(id string) ([]domain.ProductLocation, error) {
+	var productLocations []domain.ProductLocation
+	err := r.db.Select(productLocations, "SELECT * FROM product_locations WHERE location_id=$1", id)
+	if err != nil {
+		return nil, err
+	}
+
+	return productLocations, nil
+}
+
+// FindProductLocationByProductAndLocationID finds a single product location by both a product
+// and location ID, returning an error if nothing is found.
+func (r *ProductLocationRepository) FindProductLocationByProductAndLocationID(productID, locationID string) (*domain.ProductLocation, error) {
+	var productLocation *domain.ProductLocation
+	err := r.db.Get(productLocation, "SELECT * FROM product_locations WHERE product_id=$1 AND location_id=$2", productID, locationID)
+	if err != nil {
+		return nil, err
+	}
+
+	return productLocation, nil
+}
+
+// InsertProductLocation inserts a single product location into the database,
+// returning the ID on success.
+func (r *ProductLocationRepository) InsertProductLocation(productLocation domain.ProductLocation) (string, error) {
+	id := uuid.Generate()
+	_, err := r.db.Exec("INSERT INTO product_locations (id, location_id, url, slug, category) VALUES ($1, $2, $3, $4, $5)", id, productLocation.LocationID, productLocation.URL, productLocation.Slug, productLocation.Category)
+	if err != nil {
+		return "", err
+	}
+
+	return id, nil
+}
+
+// UpdateProductLocation updates a single product location in the database by ID.
+func (r *ProductLocationRepository) UpdateProductLocation(productLocation domain.ProductLocation) error {
+	_, err := r.db.Exec("UPDATE product_locations SET location_id=$1, url=$2, slug=$3, category=$4 WHERE id=$5", productLocation.LocationID, productLocation.URL, productLocation.Slug, productLocation.Category, productLocation.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// DeleteProductLocation deletes a single product location by ID.
+func (r *ProductLocationRepository) DeleteProductLocation(id string) error {
+	_, err := r.db.Exec("DELETE FROM product_locations WHERE id=$1", id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- services/hub/internal/database/product_repository.go
+++ services/hub/internal/database/product_repository.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"github.com/bfoody/Walmart-Scraper/domain"
+	"github.com/bfoody/Walmart-Scraper/utils/uuid"
 	"github.com/jmoiron/sqlx"
 )
 
@@ -20,10 +21,41 @@ func NewProductRepository(db *sqlx.DB) *ProductRepository {
 // FindProductByID finds a single product by ID, returning an error if nothing is found.
 func (r *ProductRepository) FindProductByID(id string) (*domain.Product, error) {
 	var product *domain.Product
-	err := r.db.Get(&product, "SELECT * FROM products WHERE id=$1", id)
+	err := r.db.Get(product, "SELECT * FROM products WHERE id=$1", id)
 	if err != nil {
 		return nil, err
 	}
 
 	return product, nil
+}
+
+// InsertProduct inserts a single product into the database, returning the ID on success.
+func (r *ProductRepository) InsertProduct(product domain.Product) (string, error) {
+	id := uuid.Generate()
+	_, err := r.db.Exec("INSERT INTO products (id, common_name) VALUES ($1, $2)", id, product.CommonName)
+	if err != nil {
+		return "", err
+	}
+
+	return id, nil
+}
+
+// UpdateProduct updates a single product in the database by ID.
+func (r *ProductRepository) UpdateProduct(product domain.Product) error {
+	_, err := r.db.Exec("UPDATE products SET common_name = $1 WHERE id = $2", product.CommonName, product.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// DeleteProduct deletes a single product by ID.
+func (r *ProductRepository) DeleteProduct(id string) error {
+	_, err := r.db.Exec("DELETE FROM products WHERE id = $1", id)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
