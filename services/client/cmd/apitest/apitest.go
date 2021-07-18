fc2d64155ad2cd4b0ef60a2b90c562fb494c6388
Tyler Lafayette <tylerlafayette@gmail.com>
2021-07-18 14:28:14 -0700

    Add parallel testing tool (#6)
    * Database connection in hub
    
    * Load config from .env optionally
    
    * Add .env.example
    
    * Add support for default config values + add Env to hub Config
    
    * [WIP] (Broken) start testing w/ repos
    
    * Add apitest util
    
    * Add headers to http client
    
    * Parallel test tool

--- services/client/cmd/apitest/apitest.go
+++ services/client/cmd/apitest/apitest.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strconv"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/bfoody/Walmart-Scraper/services/client/internal/api"
@@ -58,6 +61,47 @@ func main() {
 		}
 
 		exit(fmt.Sprintf("success: %+v", item))
+	case "get-product-parallel":
+		if len(args) < 3 {
+			exit("usage: apitest get-product-parallel <proxy list txt> <# threads>")
+		}
+
+		proxyList := args[1]
+
+		num, err := strconv.Atoi(args[2])
+		if err != nil {
+			exit("err: # threads not a number")
+		}
+
+		file, err := ioutil.ReadFile(proxyList)
+		if err != nil {
+			exit("err: proxy list file does not exist")
+		}
+
+		proxies := strings.Split(string(file), "\n")
+
+		wg := &sync.WaitGroup{}
+
+		for i := 0; i < num; i++ {
+			wg.Add(1)
+
+			go func(i int) {
+				http := api.NewHTTPClient()
+				http.SetProxy(proxies[i%len(proxies)])
+				client := walmart.NewClient(http)
+
+				item, err := client.GetItemDetails("onn-32-Class-HD-720P-Roku-Smart-LED-TV-100012589", "314022535")
+				if err != nil {
+					exit(err.Error())
+				}
+
+				fmt.Printf("success on thread %d: %+v\n", i, item)
+
+				wg.Done()
+			}(i)
+		}
+
+		wg.Wait()
 	default:
 		exit("usage: apitest <type> <args...>")
 	}
