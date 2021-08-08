64e8c7ccfc8c292c29c8496891dfedf57f5955f2
Tyler Lafayette <tylerlafayette@gmail.com>
2021-08-08 13:57:30 -0700

    Working item recommendations method with test

--- services/client/internal/api/walmart/client_test.go
+++ services/client/internal/api/walmart/client_test.go
@@ -23,3 +23,25 @@ func TestGetItemDetails(t *testing.T) {
 		t.Errorf("expected item.Name == %s, got %s", testItemName, item.Name)
 	}
 }
+
+// TestGetItemRecommendations tests the GetItemRelatedItems scraping method.
+func TestGetItemRecommendations(t *testing.T) {
+	c := walmart.NewClient(api.NewHTTPClient())
+	item, err := c.GetItemDetails("onn-32-Class-HD-720P-Roku-Smart-LED-TV-100012589", "314022535")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if item.Name != testItemName {
+		t.Errorf("expected item.Name == %s, got %s", testItemName, item.Name)
+	}
+
+	items, err := c.GetItemRelatedItems(item.ID, item.CategoryID, item.Category, item.Name)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(items) < 1 {
+		t.Error("`items` array is empty")
+	}
+}
